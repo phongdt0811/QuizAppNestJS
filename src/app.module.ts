4f66ce2b8e96ae568abb07caa29893f2cbc0b6da
Phong Do <dtphong0811@gmail.com>
2024-03-07 17:56:53 +0700

    add: config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizController } from './quiz/quiz.controller';
 import { QuizService } from './quiz/quiz.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, QuizController],
   providers: [AppService, QuizService],
 })
