992e19bb1c93e82e768fcc9732b32de81e876734
Phong Do <dtphong0811@gmail.com>
2024-03-08 04:58:58 +0700

    fix: enable Cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+require('dotenv').config()
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  app.enableCors();
+  await app.listen(3005);
 }
 bootstrap();
