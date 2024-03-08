89e1b2b1ead257d20c09ccda36be66894cf89484
Phong Do <dtphong0811@gmail.com>
2024-03-08 12:00:56 +0700

    add: add https

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 require('dotenv').config()
+// import * as fs from 'fs';
 
+// const httpsOptions = {
+//   key: fs.readFileSync('server.key'),
+//   cert: fs.readFileSync('server.crt'),
+// };
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule
+    // , { httpsOptions }
+    );
   app.enableCors();
   await app.listen(3005);
 }
